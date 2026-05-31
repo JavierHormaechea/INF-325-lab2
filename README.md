ea8d4a1675545a281ce9e6ed35b3aed9a42e5ad6
JavierHormaechea <89490485+JavierHormaechea@users.noreply.github.com>
2026-05-30 21:50:23 -0400

    added script to pre-process documents
    El script esta dando un error falta arreglarlo y agregar la parte para subirlos a la BD

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # INF-325-lab2
+instalar sentence transformer: pip install -U sentence-transformers
+instalar getenv: pip install getenv
---
+++ scripts/fill_DB.py
@@ -0,0 +1,26 @@
+import hashlib
+from pathlib import Path
+from sentence_transformers import SentenceTransformer
+import os
+from dotenv import load_dotenv
+
+# Esta funcion recibe el path del documento y retorna los 3 parametros a almacenar en la base de datos hash, texto y embedding
+def process_documents(file_path, model):
+    h = hashlib.sha256()
+    file = open(file_path, "r", encoding="utf-8")
+    text = ""
+    for line in file:
+        h.update(line)
+        text += line
+    file.close()
+    return h.hexdigest(), text, model.encode(text)
+    
+load_dotenv()
+
+model = SentenceTransformer('hiiamsid/sentence_similarity_spanish_es')
+
+folder = Path(os.getenv("FILES_PATH"))
+
+for file in folder.glob("*.txt"):
+    file_id, file_text, file_embedding = process_documents(file, model)
+    print(file_id)
